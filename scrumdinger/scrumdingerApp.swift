8e2297f00e6e23768dd63e4d84ef4982b49c71b6
Hayden Campbell <haydencampbell@live.com.au>
2023-03-02 13:07:55 +0700

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Hayden Campbell on 2/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
